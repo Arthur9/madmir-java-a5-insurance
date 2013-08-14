d222173a334d45a86ae8f96fb372c2b6361ef676
Arthur9 <jawzproductions@hotmail.com>
2013-08-14 11:23:02 -0400

    here's my insurance program.

--- src/InsurancePremium.java
+++ src/InsurancePremium.java
@@ -8,16 +8,29 @@
 // Methods adhere to program requirements	/5
 // Value returned from calculatePremium()	/3
 // Coding conventions & comments				/6
-
+import javax.swing.JOptionPane;
 
 public class InsurancePremium {
 
 	public static void main(String[] args) {
-		//Insert body of main() method here
+		// Declare Variables
+		int curYear, birthYear, prem;
+		
+		//User Inputs
+		curYear = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the current year: "));
+		birthYear = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter your birthyear: "));
+		
+		prem = calculatePremium(curYear, birthYear);
+		JOptionPane.showMessageDialog(null, "Your insurance premium is $" + prem);
+		
 	}
 	
-	public static int calculatePremium(/*Insert parameters here*/) {
+	public static int calculatePremium(int cy, int, int by) {
 		//Insert body of calculatePremium() method here
+		int age = cy-by;
+		int decades = age/10;
+		int premium = (decades + 15) * 20;
+		return premium
 	}
 
 }
